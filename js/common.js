0dbe038ffb0da0ca62b66bf9824998b461f33933
miseon <miseon920@naver.com>
2022-08-29 15:47:34 +0900

    20220823

--- js/common.js
+++ js/common.js
@@ -1,7 +1,31 @@
 $(function () {
   $(".top_close_btn").on("click", function () {
     // $(".TopBanner").slideUp(600);
-    $(".TopBanner").addClass("on");
+    $(".TopBanner,.Wrap").addClass("check");
   });
-  $(".main_slider").slick({});
+  const win_w = $(window).width();
+  $(window).on("scroll", function () {
+    scroll();
+  });
+  $(window).resize(function () {
+    scroll();
+  });
+  $(".main_slider").slick({
+    arrows: false,
+  });
 });
+
+function scroll() {
+  const win_w = $(window).width();
+  const scrollT = $(window).scrollTop();
+  //console.log(scrollT);
+  if (win_w > 1024) {
+    if (scrollT > 0) {
+      $(".Header").addClass("on");
+    } else {
+      $(".Header").removeClass("on");
+    }
+  } else {
+    $(".Header").removeClass("on");
+  }
+}
