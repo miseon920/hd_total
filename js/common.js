a2b9208e1d6d99f5794a7263ff5d1e618f7d239b
miseon <miseon920@naver.com>
2022-08-30 16:02:43 +0900

    20220830

--- js/common.js
+++ js/common.js
@@ -11,8 +11,24 @@ $(function () {
   $(window).resize(function () {
     scroll();
   });
+  const li_box = $(".Section01 .slide_dots > li");
+  $(".main_slider").on("init afterChange", function (e, s, c) {
+    //console.log(s.slideCount);
+    const current = $(".main_slider .slick-current");
+    //console.log(current);
+    current.addClass("on").siblings().removeClass("on");
+    $(".Section01 .num").html(
+      "<b> 0" + ((c ? c : 0) + 1) + "</b> <span> / 0" + s.slideCount + "</span>"
+    );
+    $(li_box).eq(c).addClass("on").siblings().removeClass("on");
+  });
   $(".main_slider").slick({
     arrows: false,
+    usetransform: false,
+  });
+  $(li_box).click(function () {
+    var slideNo = $(this).index();
+    $(".main_slider").slick("slickGoTo", slideNo);
   });
   $(".left_slider").slick({
     arrows: false,
@@ -36,7 +52,7 @@ $(function () {
     const txt_box = $(".Section04 .content_box > div");
     //console.log(current);
     current.addClass("on").siblings().removeClass("on");
-    $(".num").html(
+    $(".Section04 .num").html(
       "<b> 0" + ((c ? c : 0) + 1) + "</b> <span> / 0" + s.slideCount + "</span>"
     );
     $(txt_box).eq(c).addClass("on").siblings().removeClass("on");
@@ -45,12 +61,19 @@ $(function () {
     arrows: false,
     centerMode: true,
     centerPadding: "300px",
+    dots: true,
   });
-  $(".Section04 .slide_handler i:first-child").on("click", function () {
-    $(".center_slider").slick("slickPrev");
+  $(".slide_handler2 i:first-child").on("click", function () {
+    $(this)
+      .parent(".slide_handler")
+      .siblings(".slick-slider")
+      .slick("slickPrev");
   });
-  $(".Section04 .slide_handler i:last-child").on("click", function () {
-    $(".center_slider").slick("slickNext");
+  $(".slide_handler2 i:last-child").on("click", function () {
+    $(this)
+      .parent(".slide_handler")
+      .siblings(".slick-slider")
+      .slick("slickNext");
   });
 });
 
